4ea707cea6fd7d9330a59a35844d771c30310074
Iftikhar2425 <iftikharalisyed001@gmail.com>
2026-02-12 01:33:24 +0500

    Create upload and output directories if they don't exist

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 OUTPUT_FOLDER = os.path.join(BASE_DIR, "outputs")
 ORIGINAL_PDF = os.path.join(UPLOAD_FOLDER, "original.pdf")
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 # ==========================
